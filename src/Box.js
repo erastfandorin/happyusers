2bc44f80686286d58bfb09e675458382d891290e
Oleksandr Khomenko <homenkosasha13@gmail.com>
2023-03-29 13:05:04 +0300

    fix: temporary fix chrome first two click bug

--- src/Box.js
+++ src/Box.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useGLTF } from "@react-three/drei";
 import { useControls } from "leva";
 import { useSpring, animated } from "@react-spring/three";
@@ -12,7 +12,6 @@ export default function Box(props) {
     rotation: [0, 0, 0],
     config: { mass: 1, tension: 120, friction: 26 },
   }));
-
   const handleClick = () => {
     setIsBoxOpen((prevState) => !prevState);
     api.start({ position: isBoxOpen ? [0, -50, -300] : [0, 0, 0], rotation: isBoxOpen ? [-1.5, 0, 0] : [0, 0, 0] });
@@ -26,9 +25,15 @@ export default function Box(props) {
   transparentMaterial.transparent = true;
   transparentMaterial.opacity = opacity.opacity;
 
+  // temporary fix chrome warning 
+  // Event.path is deprecated and will be removed. Please use Event.composedPath() instead.
+  useEffect(() => {
+    handleClick();
+  }, [])
+
   return (
     <group {...props} dispose={null}>
-      <group rotation={[Math.PI / 2, 0, 0]} scale={0.01} position={[-1,0,0]}>
+      <group rotation={[Math.PI / 2, 0, 0]} scale={0.01} position={[-1,0,0]} >
         <mesh geometry={nodes.BOX_Bottom0.geometry} material={materials.Box_main} />
         <mesh geometry={nodes.BOX_Detail0.geometry} material={materials.Detail} />
         <mesh geometry={nodes.BOX_Handle_0.geometry} material={materials.Bottom_transp} />
