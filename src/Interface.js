b4a35ebb5da58cbdf0037ec71c672cfa8d0c4e3d
Oleksandr Khomenko <homenkosasha13@gmail.com>
2023-03-29 14:22:22 +0300

    modified: tooltip text

--- src/Interface.js
+++ src/Interface.js
@@ -2,7 +2,7 @@ export default function Interface({handleClick}) {
   return (
     <div className="interface">
       <p className="tooltip">
-        For toggle box click on box cover or this button 
+        For toggle box cover click on box cover or this button 
       </p>
       <button className="btn" onClick={() => handleClick()}>Toggle box cover</button>
     </div>
